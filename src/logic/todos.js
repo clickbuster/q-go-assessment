f9d1cdee5f661532336a90699e6cd4345b531f61
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2018-03-22 16:47:20 +0200

    ADDED additional reducer actions, extended a bit the default state

--- src/logic/todos.js
+++ src/logic/todos.js
@@ -1,12 +1,17 @@
 export const ADD_ITEM = 'qgo/assessment/ADD_ITEM';
+export const DELETE_ITEM = 'qgo/assessment/DELETE_ITEM';
+export const TOGGLE_ITEM_COMPLETED = 'qgo/assessment/TOGGLE_ITEM_COMPLETED';
+export const TOGGLE_FILTER_COMPLETED = 'qgo/assessment/TOGGLE_FILTER_COMPLETED';
 
-export const addItem = content => {
-  return { type: ADD_ITEM, content };
-};
+export const addItem = content => ({ type: ADD_ITEM, content });
+export const deleteItem = id => ({ type: DELETE_ITEM, id });
+export const toggleItemCompleted = id => ({ type: TOGGLE_ITEM_COMPLETED, id });
+export const toggleFilterCompleted = () => ({ type: TOGGLE_FILTER_COMPLETED });
 
 export const initialState = {
+  filterCompleted: false,
   items: [
-    { id: 1, content: 'Call mum' },
+    { id: 1, content: 'Call mum', completed: true },
     { id: 2, content: 'Buy cat food' },
     { id: 3, content: 'Water the plants' },
   ],
@@ -26,6 +31,36 @@ const reducer = (state = initialState, action) => {
         ...state,
         items: [...state.items, newItem],
       };
+
+    case DELETE_ITEM:
+      const index = state.items.findIndex(i => i.id === action.id);
+      state.items.splice(index, 1);
+
+      return {
+        ...state,
+        items: [...state.items]
+      };
+
+    case TOGGLE_ITEM_COMPLETED:
+      const itemIndex = state.items.findIndex(i => i.id === action.id);
+      const item = state.items[itemIndex];
+
+      state.items[itemIndex] = {
+        ...item,
+        completed: !item.completed
+      };
+
+      return {
+        ...state,
+        items: [...state.items]
+      };
+
+    case TOGGLE_FILTER_COMPLETED:
+      return {
+        ...state,
+        filterCompleted: !state.filterCompleted
+      };
+  
     default:
       return state;
   }
